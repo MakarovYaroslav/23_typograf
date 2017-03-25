2f1c7b5094acbdf52fa1e9bdb776f87a801dc634
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-03-25 15:45:16 +0300

    Добавлена обработка текста при нажатии на кнопку

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from typograph import typograph
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def form():
-    return render_template('form.html')
+    if request.method == 'POST':
+        input_text = request.form.getlist('text')[0]
+        return render_template(
+            'form.html',
+            text_after_typograph=typograph(input_text),
+            original_text=input_text)
+    else:
+        return render_template('form.html')
+
 
 if __name__ == "__main__":
     app.run()
