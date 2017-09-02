f8860c1d8f117e4b2519bd38d9a850dcfba4fa3c
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-09-02 16:36:50 +0300

    Добавлена проверка на ввод пустой строки

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def form():
     if request.method == 'POST':
-        input_text = request.form.getlist('text')[0]
+        input_text = request.form.get('text')
+        original_text = input_text
+        if not input_text:
+            input_text = None
         return render_template(
             'form.html',
             text_after_typograph=typograph(input_text),
-            original_text=input_text)
+            text_before_typograph=original_text)
     else:
         return render_template('form.html')
 
